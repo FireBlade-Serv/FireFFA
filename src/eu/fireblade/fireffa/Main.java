62b6e85b6b7dbfcbcc90e0bdd162f8a6b5510a21
batda <batda@MSI>
2017-08-10 20:38:30 +0200

    ALPHA v1.0

--- src/eu/fireblade/fireffa/Main.java
+++ src/eu/fireblade/fireffa/Main.java
@@ -1,5 +1,22 @@
 package eu.fireblade.fireffa;
 
-public class Main {
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	public static Plugin plugin;
+	
+	@Override
+	public void onEnable() {
+		getLogger().info("FireFFA ON");
+		
+		getServer().getPluginManager().registerEvents(new Events(), this);
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("FireFFA OFF");
+	}
 
 }
