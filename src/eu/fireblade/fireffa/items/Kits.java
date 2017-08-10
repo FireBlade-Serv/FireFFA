a0b1aee6a9f356f91778f6fbcf1dbf7fc8ae0bbf
Goldocelot <nico.19.9.2000@hotmail.com>
2017-08-10 22:03:16 +0200

    Methode ItemGen

--- src/eu/fireblade/fireffa/items/Kits.java
+++ src/eu/fireblade/fireffa/items/Kits.java
@@ -1,9 +1,50 @@
 package eu.fireblade.fireffa.items;
 
+import java.util.Arrays;
+
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Kits {
-	public static void ItemGen(Material m, String t) {
-		
+	public static void ItemGen0(Material m, String n, String l1, String l2, String l3) {
+		ItemStack item = new ItemStack(m);
+		ItemMeta itemM = item.getItemMeta();
+		itemM.setDisplayName(n);
+		itemM.setLore(Arrays.asList(l1, l2, l3));
+		item.setItemMeta(itemM);
+	}
+	
+	public static void ItemGen1(Material m, Enchantment ench, int level, String n, String l1, String l2, String l3) {
+		ItemStack item = new ItemStack(m);
+		ItemMeta itemM = item.getItemMeta();
+		itemM.setDisplayName(n);
+		itemM.setLore(Arrays.asList(l1, l2, l3));
+		itemM.addEnchant(ench, level, true);
+		item.setItemMeta(itemM);
+	}
+	
+	public static void ItemGen2(Material m, Enchantment ench, int level, Enchantment ench2, int level2, String n, String l1, String l2, String l3) {
+		ItemStack item = new ItemStack(m);
+		ItemMeta itemM = item.getItemMeta();
+		itemM.setDisplayName(n);
+		itemM.setLore(Arrays.asList(l1, l2, l3));
+		itemM.addEnchant(ench, level, true);
+		itemM.addEnchant(ench2, level2, true);
+		item.setItemMeta(itemM);
+	}
+	
+	public static void ItemGen3(Material m, Enchantment ench, int level, Enchantment ench2, int level2, Enchantment ench3,
+			int level3, String n, String l1, String l2, String l3) {
+		ItemStack item = new ItemStack(m);
+		ItemMeta itemM = item.getItemMeta();
+		itemM.setDisplayName(n);
+		itemM.setLore(Arrays.asList(l1, l2, l3));
+		itemM.addEnchant(ench, level, true);
+		itemM.addEnchant(ench2, level2, true);
+		itemM.addEnchant(ench3, level3, true);
+		item.setItemMeta(itemM);
 	}
 }
+	
