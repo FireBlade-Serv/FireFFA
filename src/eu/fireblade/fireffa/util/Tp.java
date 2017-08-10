0c95cc5bb179d71839dea88bd35dbed4e2e9a704
Glowstoner <glowstoner1@gmail.com>
2017-08-10 21:38:12 +0200

    TpSpawn / Added Location

--- src/eu/fireblade/fireffa/util/Tp.java
+++ src/eu/fireblade/fireffa/util/Tp.java
@@ -1,11 +1,14 @@
 package eu.fireblade.fireffa.util;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 public class Tp {
 	
 	public static void tpSpawn(Player p){
+		//tp Glowstoner 80 114 -47 90 180
 		
+		p.teleport(new Location(p.getWorld(), 80, 114, -47, 90, 180));
 	}
 
 }
