708ec188cb745c64834424aaaea536cdc73d6344
Goldocelot <nico.19.9.2000@hotmail.com>
2017-08-11 19:54:42 +0200

    ArrayList

--- src/eu/fireblade/fireffa/Var.java
+++ src/eu/fireblade/fireffa/Var.java
@@ -7,5 +7,33 @@ import org.bukkit.entity.Player;
 public class Var {
 	
 	public static ArrayList<Player> démolisseur = new ArrayList<Player>();
+	
+	public static ArrayList<Player> fantôme = new ArrayList<Player>();
+	
+	public static ArrayList<Player> tank = new ArrayList<Player>();
+	
+	public static ArrayList<Player> flic = new ArrayList<Player>();
+	
+	public static ArrayList<Player> magicien = new ArrayList<Player>();
+	
+	public static ArrayList<Player> chevalier = new ArrayList<Player>();
+	
+	public static ArrayList<Player> cactus = new ArrayList<Player>();
+	
+	public static ArrayList<Player> piaf = new ArrayList<Player>();
+	
+	public static ArrayList<Player> voleurdame = new ArrayList<Player>();
+	
+	public static ArrayList<Player> OITCman = new ArrayList<Player>();
+	
+	public static ArrayList<Player> lapin = new ArrayList<Player>();
+	
+	public static ArrayList<Player> russe = new ArrayList<Player>();
+	
+	public static ArrayList<Player> grampa = new ArrayList<Player>();
+	
+	public static ArrayList<Player> mineur = new ArrayList<Player>();
+	
+	public static ArrayList<Player> jihadist = new ArrayList<Player>();	
 
 }
