531b0dc71b26f801f13ade070975ab154483a6eb
Glowstoner <glowstoner1@gmail.com>
2017-08-11 18:58:40 +0200

    Alpha v1.2.2

--- src/eu/fireblade/fireffa/cmd/Kit.java
+++ src/eu/fireblade/fireffa/cmd/Kit.java
@@ -1,5 +1,8 @@
 package eu.fireblade.fireffa.cmd;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -11,6 +14,8 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.Potion;
+import org.bukkit.potion.PotionType;
 
 public class Kit implements CommandExecutor {
 
@@ -19,6 +24,7 @@ public class Kit implements CommandExecutor {
 		
 		if(!(sender instanceof Player)){
 			sender.sendMessage("§cVous devez être un joueur pour pouvoir executer cette commande !");
+			return false;
 		}
 		
 		Player p = (Player) sender;
@@ -37,10 +43,17 @@ public class Kit implements CommandExecutor {
 		
 		//membre
 		inv.setItem(9, genPerspective(Material.STAINED_GLASS, "§f§lKits Membre", (byte) 0));
+		inv.setItem(10, genPerspectiveEnch(Material.GOLD_HELMET, "§9Démolisseur", Enchantment.PROTECTION_ENVIRONMENTAL, 1, (byte) 0));
 		inv.setItem(10, genPerspectiveEnch(Material.GOLD_HELMET, "§9Tank", Enchantment.PROTECTION_ENVIRONMENTAL, 1, (byte) 0));
 		
 		//fire
 		inv.setItem(18, genPerspectiveEnch(Material.STAINED_GLASS, "§e§lKits Fire",Enchantment.PROTECTION_ENVIRONMENTAL, 1, (byte) 4));
+		
+		if(p.hasPermission("fireffa.fire") || p.isOp()){
+			inv.setItem(19, genPerspectivePot("§9Fantôme", PotionType.INVISIBILITY, false));
+		}else{
+			inv.setItem(19, genPerspectivePot("§cFantôme", PotionType.INVISIBILITY, false, (ArrayList<String>) Arrays.asList("§8Vous devez avoir le grade Fire !")));
+		}
 		
 		//ultra
 		inv.setItem(27, genPerspectiveEnch(Material.STAINED_GLASS, "§6§lKits Ultra",Enchantment.PROTECTION_ENVIRONMENTAL, 1, (byte) 1));
@@ -81,6 +94,31 @@ public class Kit implements CommandExecutor {
 		meta.setDisplayName(name);
 		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES, ItemFlag.HIDE_ENCHANTS);
 		item.setItemMeta(meta);
+		return item;
+	}
+	
+	private static ItemStack genPerspectivePot(String name, PotionType pt, boolean setsplash){
+		Potion p = new Potion(pt);
+		p.setSplash(setsplash);
+		
+		ItemStack item = p.toItemStack(1);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(name);
+		item.setItemMeta(meta);
+		
+		return item;
+	}
+	
+	private static ItemStack genPerspectivePot(String name, PotionType pt, boolean setsplash, ArrayList<String> list){
+		Potion p = new Potion(pt);
+		p.setSplash(setsplash);
+		
+		ItemStack item = p.toItemStack(1);
+		ItemMeta meta = item.getItemMeta();
+		meta.setDisplayName(name);
+		meta.setLore(list);
+		item.setItemMeta(meta);
+		
 		return item;
 	}
 
