b80314a551a9a1fbbb61c35449bd17555736226d
Glowstoner <glowstoner1@gmail.com>
2017-08-13 18:42:49 +0200

    Alpha v1.18.3 / Patch Scoreboard

--- src/eu/fireblade/fireffa/util/Scoreboard.java
+++ src/eu/fireblade/fireffa/util/Scoreboard.java
@@ -11,17 +11,17 @@ public class Scoreboard {
 		
 		gs.setLine("§8  ", 1);
 		gs.setLine("§7§lGrade :", 2);
-		gs.setLine("§7 Serveur §8§l>> ", 3);
-		gs.setLine("§7 FFA §8§l>> ", 4);
+		gs.setLine("§7 Serveur §8§l>> §c"+null, 3);
+		gs.setLine("§7 FFA §8§l>> §c"+null, 4);
 		gs.setLine("§6  ", 5);
 		gs.setLine("§7§lKills :", 6);
-		gs.setLine("§8§l >> ", 7);
+		gs.setLine("§8§l >> §c"+null, 7);
 		gs.setLine("§7  ", 8);
 		gs.setLine("§7§lKills Restants :", 9);
-		gs.setLine("§8§l >> ", 10);
+		gs.setLine("§8§l >> §c"+null, 10);
 		gs.setLine("§1   ", 11);
 		gs.setLine("§7§lKillStreak :", 12);
-		gs.setLine("§8§l >> ", 13);
+		gs.setLine("§8§l >> §c"+null, 13);
 		gs.setLine("§a   ", 14);
 		gs.setLine("§eplay.fireblade-serv.eu", 15);
 		
