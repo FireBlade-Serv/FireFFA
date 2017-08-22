4dab2d5e9a8d97ca0500acc221359a3b5460c583
Goldocelot <nico.19.9.2000@hotmail.com>
2017-08-22 18:44:42 +0200

    2

--- src/eu/fireblade/fireffa/ability/Ogre.java
+++ src/eu/fireblade/fireffa/ability/Ogre.java
@@ -4,6 +4,7 @@ package eu.fireblade.fireffa.ability;
 import java.util.Random;
 
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -14,6 +15,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 import eu.fireblade.fireffa.Var;
 import eu.fireblade.fireffa.items.Kits;
+import net.md_5.bungee.api.ChatColor;
 
 public class Ogre implements Listener {
   
@@ -32,6 +34,10 @@ public class Ogre implements Listener {
 				int rn = 0 + r.nextInt(100);
 				if (rn <= 15) {
 					tp.damage(6.25);
+					dp.sendMessage(ChatColor.GOLD+"§6[§eFireFFA§6] "+ChatColor.RED+"Vous avez infligé un gros coup.");
+					dp.playSound(dp.getLocation(), Sound.GHAST_DEATH, 30, 30);
+					tp.sendMessage(ChatColor.GOLD+"§6[§eFireFFA§6] "+ChatColor.RED+"Le joueur "+ dp.getName() +"vous a infligé un gros coup.");
+					tp.playSound(dp.getLocation(), Sound.GHAST_DEATH, 30, 30);
 				}
 			}
 		}
