85084b97bc804c7733101b2702d17d94ab5589dc
Glowstoner <glowstoner1@gmail.com>
2017-08-26 18:14:30 +0200

    Alpha v1.32.10

--- src/eu/fireblade/fireffa/ability/Démolisseur.java
+++ src/eu/fireblade/fireffa/ability/Démolisseur.java
@@ -1,5 +1,6 @@
 package eu.fireblade.fireffa.ability;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.bukkit.Bukkit;
@@ -33,6 +34,7 @@ import net.minecraft.server.v1_8_R3.PacketPlayOutWorldParticles;
 public class Démolisseur implements Listener {
 	
 	public static HashMap<Player, Integer> tasks = new HashMap<Player, Integer>();
+	public static ArrayList<Player> inLoad = new ArrayList<Player>();
 
 	@EventHandler
 	public void onInteract (PlayerInteractEvent e) {
@@ -67,7 +69,9 @@ public class Démolisseur implements Listener {
 				
 				if(!tasks.containsKey(p)) {
 					tasks.put(p, 0);
-				}else {
+				}
+				
+				if(inLoad.contains(p)) {
 					p.playSound(p.getLocation(), Sound.ITEM_BREAK, 30, 30);
 					p.sendMessage(ChatColor.GOLD+"§6[§eFireFFA§6] "+ChatColor.RED+"Vous avez déjà une boulle de feu en execution !");
 					
@@ -92,6 +96,8 @@ public class Démolisseur implements Listener {
 				
 				w.playSound(ar.getLocation(), Sound.ENDERMAN_SCREAM, 30, 30);
 				
+				inLoad.add(p);
+				
 				tasks.replace(p, Bukkit.getScheduler().scheduleSyncRepeatingTask(Main.plugin, new Runnable() {
 
 					@Override
@@ -99,6 +105,8 @@ public class Démolisseur implements Listener {
 						if(ar.isDead() || ar.equals(null) || ((CraftEntity)ar).getHandle().onGround) {
 							ar.remove();
 							truc.remove();
+							
+							inLoad.remove(p);
 							
 							Bukkit.getScheduler().cancelTask(tasks.get(p));
 						}else {
@@ -108,7 +116,6 @@ public class Démolisseur implements Listener {
 							for(Player online : Bukkit.getOnlinePlayers()) {
 								((CraftPlayer)online).getHandle().playerConnection.sendPacket(ppowp);
 							}
-							
 						}
 					}
 					
